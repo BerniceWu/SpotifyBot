19d83877a843e1469a297420b1874fba568c9a71
BerniceWu <bcrazy17824@gmail.com>
2018-01-01 13:57:07 +0800

    Initial project structure

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+
+
+class Config:
+    TELEGRAM_API_TOKEN = os.environ.get("TELEGRAM_API_TOKEN")
+    SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
+    SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
+    WEBHOOK_URL = os.environ.get("WEBHOOK_URL")
+
+
+class DevelopmentConfig(Config):
+    from credentials import (
+        TELEGRAM_API_TOKEN,
+        SPOTIFY_CLIENT_ID,
+        SPOTIFY_CLIENT_SECRET,
+        WEBHOOK_URL
+    )
+
+
+config = {
+    "default": DevelopmentConfig,
+    "development": DevelopmentConfig
+}
