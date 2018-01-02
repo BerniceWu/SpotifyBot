8e5f6f389cf433820256cb144b790e04b85038ba
BerniceWu <bcrazy17824@gmail.com>
2018-01-02 22:03:00 +0800

    Generate spotify API token

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ class Config:
     TELEGRAM_API_TOKEN = os.environ.get("TELEGRAM_API_TOKEN")
     SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
     SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
+    SPOTIFY_USER_NAME = os.environ.get("SPOTIFY_USER_NAME")
+    SPOTIFY_REDIRECT_URI = os.environ.get("SPOTIFY_REDIRECT_URI")
     WEBHOOK_URL = os.environ.get("WEBHOOK_URL")
 
 
@@ -13,6 +15,8 @@ class DevelopmentConfig(Config):
         TELEGRAM_API_TOKEN,
         SPOTIFY_CLIENT_ID,
         SPOTIFY_CLIENT_SECRET,
+        SPOTIFY_USER_NAME,
+        SPOTIFY_REDIRECT_URI,
         WEBHOOK_URL
     )
 
--- manage.py
+++ manage.py
@@ -1,12 +1,25 @@
 import os
 
 from flask_script import Manager
+import spotipy.util as util
 
 from app import create_app
 
 
 app = create_app(os.environ.get("TELEGRAM_BOT_CONFIG") or "default")
 manager = Manager(app)
+
+
+@manager.command
+def get_spotify_token():
+    token = util.prompt_for_user_token(
+        app.config['SPOTIFY_USER_NAME'],
+        'user-read-playback-state user-modify-playback-state user-library-read',
+        client_id=app.config['SPOTIFY_CLIENT_ID'],
+        client_secret=app.config['SPOTIFY_CLIENT_SECRET'],
+        redirect_uri=app.config['SPOTIFY_REDIRECT_URI']
+    )
+    print('Your spotify token is:\n', token)
 
 
 if __name__ == "__main__":
